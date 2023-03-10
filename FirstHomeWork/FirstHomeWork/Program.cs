14e8e194da864654a6a7bdd7a69ac2a9861d006f
MarioLatinsky <122560458+MarioLatinsky@users.noreply.github.com>
2023-03-10 16:14:02 +0100

    Add files via upload

---
+++ FirstHomeWork/FirstHomeWork/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please enter the first number ");
+int firstInput = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number");
+int secondInput = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter the operation you'd like to do");
+char operation = Convert.ToChar(Console.ReadLine());
+
+
+
+
+if(operation ==  '-')
+{
+double result = firstInput - secondInput;
+    Console.WriteLine("The result is: " +result);
+}else if(operation == '+')
+{
+    double result = firstInput + secondInput;
+    Console.WriteLine("The result is: " + result);
+
+}
+else if(operation == '*')
+{
+    double result = firstInput * secondInput;
+    Console.WriteLine("The result is: " + result);
+}
+else if(operation == '/')
+{
+
+    double result = firstInput / secondInput;
+    Console.WriteLine("The result is: " + result);
+}
+else
+{
+    Console.WriteLine("You entered an invalid Symbol");
+}
+
+
+
+
+
+
+
+
