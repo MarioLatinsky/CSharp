ab55bb3367d2e0f0097ebaa78b8176158321b6b3
MarioLatinsky <122560458+MarioLatinsky@users.noreply.github.com>
2023-04-09 12:47:29 +0200

    Add files via upload

---
+++ C#AdvancedClass1/Homework1/Homework1/Interfaces/IAnimal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1.Interfaces
+{
+    public interface IAnimal
+    {
+
+        void printAnimal(string Type, string Name, int Age);
+
+    }
+}
---
+++ C#AdvancedClass1/Homework1/Homework1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Homework1.Enteties;
+
+Dog firstDog = new Dog("Dog", "Phill", 12, true);
+
+firstDog.printAnimal("Dog", "Phill", 12);
+firstDog.Bark();
+
+
+Cat firstCat = new Cat("Cat", "Tom", 5, true);
+
+firstCat.printAnimal("Cat", "Tom", 5);
+firstCat.Eat("Fish");
