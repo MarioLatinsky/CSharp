678cb34ee94e4f3fcd48807fdec1cdb19fe69449
MarioLatinsky <122560458+MarioLatinsky@users.noreply.github.com>
2023-04-27 14:11:34 +0200

    Add files via upload

---
+++ Class6Homework/Class6Homework/Entities/Helper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class6Homework.Entities
+{
+    public static class Helper
+
+    {
+        public static void PrintEntities<T>(this List<T> list) where T : BaseEntity
+        {
+            
+            foreach (T item in list)
+            {
+                item.Print();
+            }
+        }
+
+        public static void PrintItems<T>(this List<T> list)
+        {
+            foreach (T item in list)
+            {
+                Console.WriteLine("HHH");
+            }
+        }
+
+    }
+}
---
+++ Class6Homework/Class6Homework/Program.cs
@@ -0,0 +1,28 @@
+﻿using Class6Homework;
+using Class6Homework.Entities;
+using System.Collections.Generic;
+
+//1
+
+//List<Person> NameStartingWithR = (List<Person>)Db.people.Where(x => x.FirstName.StartsWith('R')).OrderByDescending(x => x.Age).ToList();
+
+////NameStartingWithR.PrintEntities();
+
+////2
+//List<Dog> AllBrownDogsOlderThanThree = (List<Dog>)Db.dogs.Where(x => x.Color == "Brown" && x.Age > 3).OrderBy(x => x.Age).ToList();
+////AllBrownDogsOlderThanThree.PrintEntities();
+
+////3
+//List<Person> PeopleWithMoreThanTwoDogs = (List<Person>)Db.people.Where(x => x.Dogs.Count > 2).OrderByDescending(x => x.FirstName).ToList();
+
+//PeopleWithMoreThanTwoDogs.PrintEntities();
+//PeopleWithMoreThanTwoDogs.PrintItems();
+
+List<Person> FreddysDogs = (List<Person>)Db.people.Where(x => x.FirstName == "Freddy").SelectMany(x => x.Dogs)
+                                                                                      .Where(x => x.Age > 1)
+                                                                                      .Select(x => x.Name);
+
+foreach (Person dog in FreddysDogs  )
+{
+    Console.WriteLine(dog.FirstName);
+}
