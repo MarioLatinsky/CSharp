9a069bc8ff1f4fbd33cd4ced22dab72540f393e6
MarioLatinsky <122560458+MarioLatinsky@users.noreply.github.com>
2023-04-27 14:16:16 +0200

    Add files via upload

---
+++ C#AdvancedClass4HomeWork/HomeworkClass4/Entities/FrenchCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkClass4.Entities
+{
+    public class FrenchCar : BaseEntity
+    {
+        public override void Drive(string destinationName)
+        {
+            Console.WriteLine($"This car is driving to {destinationName}");
+        }
+    }
+}
