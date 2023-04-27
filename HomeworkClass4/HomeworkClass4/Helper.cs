f707d0af919c7769fe813e89f06e41d1d756b1c5
MarioLatinsky <122560458+MarioLatinsky@users.noreply.github.com>
2023-04-27 14:13:38 +0200

    Add files via upload

---
+++ HomeworkClass4/HomeworkClass4/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkClass4
+{
+   public static class Helper
+    {
+        public static void ColorText(this string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ HomeworkClass4/HomeworkClass4/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+using HomeworkClass4;
+using HomeworkClass4.Entities;
+
+
+AmericanCar ford = new AmericanCar()
+{
+    Id = 1,
+    Brand = "Ford",
+    Model = "GT",
+    MaxSpeed = 275,
+    HorsePower = 400,
+    FuelType = "Petrol"
+};
+
+FrenchCar peugeot = new FrenchCar()
+{
+    Id = 2,
+    Brand = "Peugeot",
+    Model = "205 GTI",
+    MaxSpeed = 230,
+    HorsePower = 150,
+    FuelType = "Petrol"
+};
+
+GermanCar BMW = new GermanCar()
+{
+    Id = 3,
+    Brand = "BMW",
+    Model = "E60 M5",
+    MaxSpeed = 310,
+    HorsePower = 507,
+    FuelType = "Petrol"
+};
+
+JapaneseCar nissan = new JapaneseCar()
+{
+    Id = 4,
+    Brand = "Nissan",
+    Model = "R35 GT-R",
+    MaxSpeed = 330,
+    HorsePower = 600,
+    FuelType = "Petrol"
+};
+
+GenericDB<AmericanCar>.Insert(ford);
+GenericDB<FrenchCar>.Insert(peugeot);
+GenericDB<GermanCar>.Insert(BMW);
+GenericDB<JapaneseCar>.Insert(nissan);
+
+string text = "These are all the cars";
+
+text.ColorText(ConsoleColor.Green);
