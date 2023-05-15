ceb7428aa4eb7491396883a1909f8490b08c27b0
MarioLatinsky <122560458+MarioLatinsky@users.noreply.github.com>
2023-05-15 23:49:14 +0200

    Add files via upload

---
+++ C#AdvancedClass11/Class11Homework/Class11Homework/Program.cs
@@ -0,0 +1,51 @@
+﻿using Class11Homework;
+using Newtonsoft.Json;
+
+string folderPath = "../../../../MyData";
+string filePath = folderPath + "/myFile.json";
+
+Dog userInput = new Dog();
+Console.WriteLine("Please enter your dog's name: ");
+userInput.Name = Console.ReadLine();
+
+Console.WriteLine("Please enter your dog's Age:");
+userInput.Age = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter your dog's Color:");
+userInput.Color = Console.ReadLine();
+
+
+
+if (!Directory.Exists(folderPath))
+{
+    Directory.CreateDirectory(folderPath);
+}
+if (!File.Exists(filePath))
+{
+    File.Create(filePath).Close();
+}
+
+
+void WriteToJson(string json)
+{
+    using (StreamWriter sw = new StreamWriter(filePath))
+    {
+        sw.WriteLine(json);
+    }
+}
+
+void ReadFromJson(string json)
+{
+    using(StreamReader sr = new StreamReader(filePath))
+    {
+        Console.WriteLine(sr.ReadToEnd());
+    }
+}
+
+
+
+string dogSerialized = JsonConvert.SerializeObject(userInput);
+
+
+WriteToJson(dogSerialized);
+ReadFromJson(dogSerialized);
